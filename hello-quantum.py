a0ecfa5a40589041faaf20d4767ef618291d6e5c
Samikmalhotra <samikmalhotra66@gmail.com>
2021-10-14 11:19:07 +0530

    established entanglement

--- hello-quantum.py
+++ hello-quantum.py
@@ -1,8 +1,17 @@
 from qiskit import *
 from matplotlib import *
 circuit = QuantumCircuit(2, 2)
-
-circuit.draw(output='mpl')
+print("----------------------------------------\n")
+print(circuit.draw())
+print("----------------------------------------\n")
 
 circuit.h(0)
+print("----------------------------------------\n")
 print(circuit.draw())
+print("----------------------------------------\n")
+
+circuit.cx(0, 1)  # 0->control-qubit  1->target qubit
+circuit.measure([0, 1], [0, 1])
+print("----------------------------------------\n")
+print(circuit.draw())
+print("----------------------------------------\n")
