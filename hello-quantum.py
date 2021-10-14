c7f2c9d940e0b8777827e8afb906e684631b88fe
Samikmalhotra <samikmalhotra66@gmail.com>
2021-10-15 02:27:53 +0530

    getting real time quantum stats

--- hello-quantum.py
+++ hello-quantum.py
@@ -15,15 +15,11 @@ circuit.draw(output='mpl')
 # %%
 circuit.cx(0, 1)  # 0->control-qubit  1->target qubit
 circuit.measure([0, 1], [0, 1])
-print("----------------------------------------\n")
-print(circuit.draw())
-print("----------------------------------------\n")
+circuit.draw(output='mpl')
 
 # %%
 simulator = Aer.get_backend('qasm_simulator')
 result = execute(circuit, simulator).result()
-print("----------------------------------------\n")
 plot_histogram(result.get_counts(circuit))
-print("----------------------------------------\n")
 
 # %%
--- simulators&providers.py
+++ simulators&providers.py
@@ -1,9 +1,22 @@
 # %%
 from qiskit import *
+from qiskit.circuit.quantumregister import Qubit
+from qiskit.providers import provider
 IBMQ.save_account(open("ibmapi.txt", "r").read())
 IBMQ.load_account()
 
 # %%
 Aer.backends()
 
+# %%
+provider = IBMQ.get_provider("ibm-q")
+provider.backends()
+
+# %%
+for backend in provider.backends():
+    try:
+        qubit_count = len(backend.properties().qubits)
+    except:
+        qubit_count = "simulated"
+    print(f"{backend.name()} : {backend.status().pending_jobs} pending jobs & {qubit_count} qubits")
 # %%
