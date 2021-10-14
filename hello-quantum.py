fc0179e54b0effc2da6cfa2f5b256864066da662
Samikmalhotra <samikmalhotra66@gmail.com>
2021-10-14 20:51:51 +0530

    changed to interactive environment

--- hello-quantum.py
+++ hello-quantum.py
@@ -1,17 +1,29 @@
+# %%
+from matplotlib import *
 from qiskit import *
 from matplotlib import *
+from qiskit.visualization import plot_histogram
+
+# %%
 circuit = QuantumCircuit(2, 2)
-print("----------------------------------------\n")
-print(circuit.draw())
-print("----------------------------------------\n")
+circuit.draw(output='mpl')
 
+# %%
 circuit.h(0)
-print("----------------------------------------\n")
-print(circuit.draw())
-print("----------------------------------------\n")
+circuit.draw(output='mpl')
 
+# %%
 circuit.cx(0, 1)  # 0->control-qubit  1->target qubit
 circuit.measure([0, 1], [0, 1])
 print("----------------------------------------\n")
 print(circuit.draw())
 print("----------------------------------------\n")
+
+# %%
+simulator = Aer.get_backend('qasm_simulator')
+result = execute(circuit, simulator).result()
+print("----------------------------------------\n")
+plot_histogram(result.get_counts(circuit))
+print("----------------------------------------\n")
+
+# %%
