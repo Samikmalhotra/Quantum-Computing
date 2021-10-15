742a02a751a020fa2349ab5469e3aaaf5cc4731f
Samikmalhotra <samikmalhotra66@gmail.com>
2021-10-15 22:23:05 +0530

    ran deutsch in qasm simulator

--- deutsch-algo.py
+++ deutsch-algo.py
@@ -9,3 +9,26 @@ circuit = QuantumCircuit(2, 1)
 # %%
 circuit.h(0)
 circuit.x(1)
+circuit.h(1)
+circuit.barrier()
+circuit.draw(output='mpl')
+
+# %%
+circuit.cx(0, 1)
+circuit.barrier()
+circuit.h(0)
+circuit.barrier()
+circuit.draw(output='mpl')
+
+# %%
+circuit.measure(0, 0)
+circuit.draw(output='mpl')
+
+# %%
+backend = Aer.get_backend('qasm_simulator')
+result = execute(circuit, backend=backend, shots=1024).result()
+counts = result.get_counts(circuit)
+
+plot_histogram([counts])
+
+# %%
